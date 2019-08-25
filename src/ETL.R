26e5d802ef81b6abccc32ff6d96326c39f5d7656
ZackLarsen <zlarsen4@gmail.com>
2019-08-25 17:35:28 -0500

    Add ETL script

---
+++ src/ETL.R
@@ -0,0 +1,13 @@
+##################################################
+## Project: Divvy
+## Script purpose: Extract, Transform, Load Divvy station and trip data
+## Date: August 25, 2019
+## Author: Zack Larsen
+##################################################
+
+library(pacman)
+library(tidyverse)
+p_load(data.table, dtplyr)
+
+
+
